4376c3205099fe6a061f904c5f395ab23d4bc208
GedsonMonteiro <decleidson2@gmail.com>
2021-11-25 11:50:59 -0300

    correcao eventos

--- src/pages/Social5.js
+++ src/pages/Social5.js
@@ -9,8 +9,8 @@ function Social5() {
     <div className="home" style={{ backgroundImage: `url(${BannerSocial})` }}>
       <div className="headerContainer">
         <h1 style={{fontSize: 35, display: 'flex',  justifyContent:'center', alignItems:'center'}}> FICR no mercado tecnológico </h1>
-        <p style={{fontSize: 20, color: 'black' }}> Segunda, 7 de março de 2022, às 18h30</p>
-        <p style={{fontSize: 20, color: 'black' }}> Local: Avenida Caxangá, 3841 - Iputinga - Recife</p>
+        <p style={{fontSize: 20, color: 'white' }}> Segunda, 7 de março de 2022, às 18h30</p>
+        <p style={{fontSize: 20, color: 'white' }}> Local: Avenida Caxangá, 3841 - Iputinga - Recife</p>
         <p style={{fontSize: 15, color: 'white' }}> Entrada: gratuita</p>
         <p style={{fontSize: 15, color: 'white' }}> O evento proporcionará aos universitários adquirir e por em práticas conhecimentos vistos em sala de aula, além de terem oportunidades de realizar novos negócios com empresas de destaque no mercado na região metropolitana.</p>
         <SocialFollow />
